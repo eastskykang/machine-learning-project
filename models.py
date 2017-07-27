6e197d1d86f0418d8a0ce0e8923e122169b11a48
vwegmayr <vwegmayr@inf.ethz.ch>
2017-07-27 18:54:42 +0200

    Add first ideas for data handling

--- models.py
+++ models.py
@@ -1,19 +1,43 @@
 import numpy as np
 import sklearn as skl
+import pickle
 
 class RandomSelection(skl.base.BaseEstimator, skl.base.TransformerMixin):
 
-    def __init__(self, n_components=1000):
+    def __init__(self, n_components=1000, random_state=None):
         self.n_components = n_components
+        self.random_state = random_state
 
-    def fit(self, X):
+        self.components = None
+
+    def fit(self, data):
         pass
 
-    def fit_transform(self, X):
+    def transform(self, data):
+        pass
+
+    def fit_transform(self, data):
         pass
 
     def get_params(self):
         pass
 
     def set_params(self):
-        passgit push 
+        pass
+
+
+def load_data(path):
+    return pickle.load(open(path, "rb"))
+
+
+def save_data(data, path):
+     return pickle.dump(data, open(path, "wb"))
+
+
+class Data(object):
+    """docstring for Data"""
+    def __init__(self, X=None, y=None, pipeline=None):
+        self.X = X
+        self.y = y
+        self.pipeline = pipeline
+        
--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 from config import Config
+import nibabel as nib
+import numpy as np
+import hashlib
 
+X = np.load("X_test.npy")
+print(hashlib.md5(X).hexdigest())
+
+X[0,0]=1
+print(hashlib.md5(X).hexdigest())
