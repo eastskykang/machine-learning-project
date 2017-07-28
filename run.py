310f7d0b6513cf08f7a4016740dbd5d3f13e9aec
vwegmayr <vwegmayr@inf.ethz.ch>
2017-07-28 10:03:44 +0200

    Add first basic setup

--- run.py
+++ run.py
@@ -2,9 +2,27 @@ from config import Config
 import nibabel as nib
 import numpy as np
 import hashlib
+import os
+import models
+import argparse
 
-X = np.load("X_test.npy")
-print(hashlib.md5(X).hexdigest())
+#y = np.loadtxt("data/y_1.csv")
 
-X[0,0]=1
-print(hashlib.md5(X).hexdigest())
+parser = argparse.ArgumentParser(description="Transformer")
+parser.add_argument("config",
+                    help="config for transformer")
+parser.add_argument("input",
+                    help="input data set")
+
+args = parser.parse_args()
+
+Config.parse_config_file(args.config)
+
+X = np.load(args.input)
+
+transformer = Config.config["class"](**Config.config["params"])
+
+X_new = transformer.fit_transform(X)
+
+
+np.save("/tmp/smt/"+Config.config["name"]+".npy", X_new)
