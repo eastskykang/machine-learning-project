4988615042a694f47eda377e9199221264ab9e41
Dongho Kang <east0822@gmail.com>
2017-10-10 23:16:47 +0200

    median feature added

--- ml_project/models/feature_extraction.py
+++ ml_project/models/feature_extraction.py
@@ -2,6 +2,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_array
 from ml_project.models import utils
 import numpy as np
+import cv2
 
 class IntensityHistogram(BaseEstimator, TransformerMixin):
     """Feature from intensity histogram of 3D images"""
@@ -14,6 +15,9 @@ class IntensityHistogram(BaseEstimator, TransformerMixin):
                  bin_number=45):
 
         # image dimension
+        self.imageDimX = utils.Constants.IMAGE_DIM_X
+        self.imageDimY = utils.Constants.IMAGE_DIM_Y
+        self.imageDimZ = utils.Constants.IMAGE_DIM_Z
         self.histBinMax = utils.Constants.IMAGE_VALUE_MAX
 
         # member variables
@@ -46,28 +50,22 @@ class IntensityHistogram(BaseEstimator, TransformerMixin):
         print("shape of X before transform : ")
         print(X.shape)
 
-        scale = n_features / utils.Constants.IMAGE_FULL_FEATURE
-
-        imageDimX = int(utils.Constants.IMAGE_DIM_X * (scale ** 1/3))
-        imageDimY = int(utils.Constants.IMAGE_DIM_Y * (scale ** 1/3))
-        imageDimZ = int(utils.Constants.IMAGE_DIM_Z * (scale ** 1/3))
-
         X_3D = np.reshape(X, (-1,
-                              imageDimX,
-                              imageDimY,
-                              imageDimZ))
+                              self.imageDimX,
+                              self.imageDimY,
+                              self.imageDimZ))
 
         # cell (contains index of voxels) as bin edge
         x_cell_edges = np.linspace(0,
-                                   imageDimX,
+                                   self.imageDimX,
                                    self.x_cell_number + 1,
                                    dtype=int)
         y_cell_edges = np.linspace(0,
-                                   imageDimY,
+                                   self.imageDimY,
                                    self.y_cell_number + 1,
                                    dtype=int)
         z_cell_edges = np.linspace(0,
-                                   imageDimZ,
+                                   self.imageDimZ,
                                    self.z_cell_number + 1,
                                    dtype=int)
 
@@ -183,6 +181,91 @@ class IntensityMean(BaseEstimator, TransformerMixin):
         return X_new
 
 
+class IntensityMedian(BaseEstimator, TransformerMixin):
+    """mean of each cell area"""
+
+    def __init__(self, x_cell_number=8, y_cell_number=8,
+                 z_cell_number=8):
+        # image dimension
+        self.imageDimX = utils.Constants.IMAGE_DIM_X
+        self.imageDimY = utils.Constants.IMAGE_DIM_Y
+        self.imageDimZ = utils.Constants.IMAGE_DIM_Z
+
+        self.x_cell_number = x_cell_number
+        self.y_cell_number = y_cell_number
+        self.z_cell_number = z_cell_number
+
+    def fit(self, X, y=None):
+        print("------------------------------------")
+        print("IntensityMedian fit")
+        print("cell numbers = {}x{}x{}".format(self.x_cell_number,
+                                               self.y_cell_number,
+                                               self.z_cell_number))
+
+        X = check_array(X)
+
+        return self
+
+    def transform(self, X, y=None):
+        print("------------------------------------")
+        print("IntensityMedian transform")
+        print("shape of X before transform : ")
+        print(X.shape)
+
+        X = check_array(X)
+        n_samples, n_features = np.shape(X)
+
+        X_3D = np.reshape(X, (-1,
+                              self.imageDimX,
+                              self.imageDimY,
+                              self.imageDimZ))
+
+        # cell (contains index of voxels) as bin edge
+        x_cell_edges = np.linspace(0,
+                                   self.imageDimX,
+                                   self.x_cell_number + 1,
+                                   dtype=int)
+        y_cell_edges = np.linspace(0,
+                                   self.imageDimY,
+                                   self.y_cell_number + 1,
+                                   dtype=int)
+        z_cell_edges = np.linspace(0,
+                                   self.imageDimZ,
+                                   self.z_cell_number + 1,
+                                   dtype=int)
+
+        # histograms
+        values = np.zeros((n_samples,
+                           self.x_cell_number,
+                           self.y_cell_number,
+                           self.z_cell_number))
+
+        for i in range(0, n_samples):
+            image_3D = X_3D[i, :, :, :]
+
+            for xi in range(0, x_cell_edges.size - 1):
+                for yi in range(0, y_cell_edges.size - 1):
+                    for zi in range(0, z_cell_edges.size - 1):
+                        # image block for histogram
+                        image_block = image_3D[
+                                      x_cell_edges[xi]:x_cell_edges[
+                                          xi + 1],
+                                      y_cell_edges[yi]:y_cell_edges[
+                                          yi + 1],
+                                      z_cell_edges[zi]:z_cell_edges[
+                                          zi + 1]]
+
+                        # mean
+                        values[i, xi, yi, zi] = np.median(image_block)
+
+        X_new = np.reshape(values, (n_samples, -1))
+
+        print("shape of X after transform : ")
+        print(X_new.shape)
+
+        return X_new
+
+
 # class IntensityGradient(BaseEstimator, TransformerMixin):
 #     """Gradient histogram for cells"""
 #     # divide 3d image into cells and make histogram per cell
@@ -281,8 +364,3 @@ class IntensityMean(BaseEstimator, TransformerMixin):
 #
 #         return X_new
 #
-
-class SIFT(BaseEstimator, TransformerMixin):
-    def __init__(self):
-
-    def fit(self, X, y=None):
