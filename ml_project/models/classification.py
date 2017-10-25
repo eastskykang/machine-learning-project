3881393251c855ee7ca6d06e5ab04b5b8426c347
vwegmayr <vwegmayr@inf.ethz.ch>
2017-10-25 16:33:43 +0200

    Add MeanPredictor

--- ml_project/models/classification.py
+++ ml_project/models/classification.py
@@ -1,7 +1,10 @@
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
 from ml_project.models.utils import probs2labels
 from sklearn.svm import SVC
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.utils.validation import check_array, check_X_y, check_is_fitted
+
 
 class SupportVectorClassifier(SVC):
     """docstring for SVM"""
@@ -12,7 +15,7 @@ class SupportVectorClassifier(SVC):
     def predict_proba(self, X):
         T = super(SupportVectorClassifier, self).predict_proba(X)
         n_samples, _ = T.shape
-        return np.hstack((T, np.zeros((n_samples, 1))))
+        return T #np.hstack((T, np.zeros((n_samples, 1))))
 
 
 class LinearDiscriminant(LinearDiscriminantAnalysis):
@@ -24,4 +27,16 @@ class LinearDiscriminant(LinearDiscriminantAnalysis):
     def predict_proba(self, X):
         T = super(LinearDiscriminant, self).predict_proba(X)
         n_samples, _ = T.shape
-        return np.hstack((T, np.zeros((n_samples, 1))))
+        return T #np.hstack((T, np.zeros((n_samples, 1))))
+
+
+class MeanPredictor(BaseEstimator, TransformerMixin):
+    """docstring for LinearDiscriminant"""
+    def fit(self, X, y):
+        self.mean = y.mean(axis=0)
+        return self
+
+    def predict_proba(self, X):
+        check_is_fitted(self, ["mean"])
+        n_samples, _ = X.shape
+        return np.tile(self.mean, (n_samples, 1))
