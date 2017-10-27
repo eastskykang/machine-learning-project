2c951d40e78d46c9bf350c9a6b60b2cfec86ae39
Dongho Kang <east0822@gmail.com>
2017-10-28 00:40:46 +0200

    IntensityHist + Sift test

--- ml_project/models/feature_extraction.py
+++ ml_project/models/feature_extraction.py
@@ -6,6 +6,34 @@ import numpy as np
 import cv2
 
 
+# class FeatureCombination(BaseEstimator, TransformerMixin):
+#     """FeatureCombination class for combine features from different class"""
+#     def __init__(self, features=None, params=None):
+#         self.feature_classes = []
+#         self.params = []
+#
+#         if features is None:
+#             pass
+#
+#         for feature in features:
+#             if feature == "ImageHistogram":
+#                 self.feature_classes.append()
+#             elif feature == "Sift":
+#                 self.feature_classes.append()
+#             elif feature == "AutoEncoder"
+#             else
+#                 assert
+#
+#
+#     def fit(self, X, y=None):
+#         # no internal variable
+#         X = check_array(X)
+#
+#     def transform(self, X, y=None):
+#         # no internal variable
+#         X = check_array(X)
+
+
 class IntensityHistogram(BaseEstimator, TransformerMixin):
     """Feature from intensity histogram of 3D images"""
 
@@ -108,323 +136,6 @@ class IntensityHistogram(BaseEstimator, TransformerMixin):
         if self.verbosity > 0:
             print("shape of X after transform : ")
             print(X_new.shape)
-
-        return X_new
-
-
-class IntensityMean(BaseEstimator, TransformerMixin):
-    """mean of each cell area"""
-
-    def __init__(self, x_cell_number=8, y_cell_number=8, z_cell_number=8):
-        # image dimension
-        self.imageDimX = utils.Constants.IMAGE_DIM_X
-        self.imageDimY = utils.Constants.IMAGE_DIM_Y
-        self.imageDimZ = utils.Constants.IMAGE_DIM_Z
-
-        self.x_cell_number = x_cell_number
-        self.y_cell_number = y_cell_number
-        self.z_cell_number = z_cell_number
-
-    def fit(self, X, y=None):
-        print("------------------------------------")
-        print("IntensityMean fit")
-        print("cell numbers = {}x{}x{}".format(self.x_cell_number,
-                                               self.y_cell_number,
-                                               self.z_cell_number))
-
-        X = check_array(X)
-
-        return self
-
-    def transform(self, X, y=None):
-        print(X.shape)
-
-        print("------------------------------------")
-        print("IntensityMean transform")
-        print("shape of X before transform : ")
-
-        X = check_array(X)
-        n_samples, n_features = np.shape(X)
-
-        X_3D = np.reshape(X, (-1,
-                              self.imageDimX,
-                              self.imageDimY,
-                              self.imageDimZ))
-
-        # cell (contains index of voxels) as bin edge
-        x_cell_edges = np.linspace(0,
-                                   self.imageDimX,
-                                   self.x_cell_number + 1,
-                                   dtype=int)
-        y_cell_edges = np.linspace(0,
-                                   self.imageDimY,
-                                   self.y_cell_number + 1,
-                                   dtype=int)
-        z_cell_edges = np.linspace(0,
-                                   self.imageDimZ,
-                                   self.z_cell_number + 1,
-                                   dtype=int)
-
-        # histograms
-        values = np.zeros((n_samples,
-                           self.x_cell_number,
-                           self.y_cell_number,
-                           self.z_cell_number))
-
-        for i in range(0, n_samples):
-            image_3D = X_3D[i, :, :, :]
-
-            for xi in range(0, x_cell_edges.size - 1):
-                for yi in range(0, y_cell_edges.size - 1):
-                    for zi in range(0, z_cell_edges.size - 1):
-
-                        # image block for histogram
-                        image_block = image_3D[
-                                      x_cell_edges[xi]:x_cell_edges[xi+1],
-                                      y_cell_edges[yi]:y_cell_edges[yi+1],
-                                      z_cell_edges[zi]:z_cell_edges[zi+1]]
-
-                        # mean
-                        values[i, xi, yi, zi] = np.average(image_block)
-
-        X_new = np.reshape(values, (n_samples, -1))
-
-        print("shape of X after transform : ")
-        print(X_new.shape)
-
-        return X_new
-
-
-class IntensityMedian(BaseEstimator, TransformerMixin):
-    """mean of each cell area"""
-
-    def __init__(self, x_cell_number=8, y_cell_number=8,
-                 z_cell_number=8):
-        # image dimension
-        self.imageDimX = utils.Constants.IMAGE_DIM_X
-        self.imageDimY = utils.Constants.IMAGE_DIM_Y
-        self.imageDimZ = utils.Constants.IMAGE_DIM_Z
-
-        self.x_cell_number = x_cell_number
-        self.y_cell_number = y_cell_number
-        self.z_cell_number = z_cell_number
-
-    def fit(self, X, y=None):
-        print("------------------------------------")
-        print("IntensityMedian fit")
-        print("cell numbers = {}x{}x{}".format(self.x_cell_number,
-                                               self.y_cell_number,
-                                               self.z_cell_number))
-
-        X = check_array(X)
-
-        return self
-
-    def transform(self, X, y=None):
-        print(X.shape)
-
-        print("------------------------------------")
-        print("IntensityMedian transform")
-        print("shape of X before transform : ")
-
-        X = check_array(X)
-        n_samples, n_features = np.shape(X)
-
-        X_3D = np.reshape(X, (-1,
-                              self.imageDimX,
-                              self.imageDimY,
-                              self.imageDimZ))
-
-        # cell (contains index of voxels) as bin edge
-        x_cell_edges = np.linspace(0,
-                                   self.imageDimX,
-                                   self.x_cell_number + 1,
-                                   dtype=int)
-        y_cell_edges = np.linspace(0,
-                                   self.imageDimY,
-                                   self.y_cell_number + 1,
-                                   dtype=int)
-        z_cell_edges = np.linspace(0,
-                                   self.imageDimZ,
-                                   self.z_cell_number + 1,
-                                   dtype=int)
-
-        # histograms
-        values = np.zeros((n_samples,
-                           self.x_cell_number,
-                           self.y_cell_number,
-                           self.z_cell_number))
-
-        for i in range(0, n_samples):
-            image_3D = X_3D[i, :, :, :]
-
-            for xi in range(0, x_cell_edges.size - 1):
-                for yi in range(0, y_cell_edges.size - 1):
-                    for zi in range(0, z_cell_edges.size - 1):
-                        # image block for histogram
-                        image_block = image_3D[
-                                      x_cell_edges[xi]:x_cell_edges[
-                                          xi + 1],
-                                      y_cell_edges[yi]:y_cell_edges[
-                                          yi + 1],
-                                      z_cell_edges[zi]:z_cell_edges[
-                                          zi + 1]]
-
-                        # mean
-                        values[i, xi, yi, zi] = np.median(image_block)
-
-        X_new = np.reshape(values, (n_samples, -1))
-
-        print("shape of X after transform : ")
-        print(X_new.shape)
-
-        return X_new
-
-
-class IntensityMeanAndMedian(BaseEstimator, TransformerMixin):
-    """Union of Mean and Median"""
-    def __init__(self, x_cell_number=8, y_cell_number=8,
-                 z_cell_number=8):
-        # image dimension
-        self.imageDimX = utils.Constants.IMAGE_DIM_X
-        self.imageDimY = utils.Constants.IMAGE_DIM_Y
-        self.imageDimZ = utils.Constants.IMAGE_DIM_Z
-
-        self.x_cell_number = x_cell_number
-        self.y_cell_number = y_cell_number
-        self.z_cell_number = z_cell_number
-
-        self.mean = IntensityMean(x_cell_number=x_cell_number,
-                                  y_cell_number=y_cell_number,
-                                  z_cell_number=z_cell_number)
-        self.median = IntensityMedian(x_cell_number=x_cell_number,
-                                      y_cell_number=y_cell_number,
-                                      z_cell_number=z_cell_number)
-
-    def fit(self, X, y=None):
-        print("------------------------------------")
-        print("IntensityMeanAndMedian fit")
-        print("cell numbers = {}x{}x{}".format(self.x_cell_number,
-                                               self.y_cell_number,
-                                               self.z_cell_number))
-
-        X = check_array(X)
-
-        return self
-
-    def transform(self, X, y=None):
-        X = check_array(X)
-
-        print("------------------------------------")
-        print("IntensityMeanAndMedian transform")
-        print("shape of X before transform : ")
-        print(X.shape)
-
-        X_mean = self.mean.transform(X, y)
-        X_median = self.median.transform(X, y)
-
-        X_new = np.hstack((X_mean, X_median))
-
-        print("shape of X after transform : ")
-        print(X_new.shape)
-
-        return X_new
-
-
-class MeanIntensityGradient(BaseEstimator, TransformerMixin):
-    """Mean Vector of Gradient for each cells"""
-    # divide 3d image into cells and make histogram per cell
-    def __init__(self,
-                 x_cell_number=8,
-                 y_cell_number=8,
-                 z_cell_number=8,
-                 bin_number=8):
-
-        # image dimension
-        self.imageDimX = utils.Constants.IMAGE_DIM_X
-        self.imageDimY = utils.Constants.IMAGE_DIM_Y
-        self.imageDimZ = utils.Constants.IMAGE_DIM_Z
-        self.histBinMax = utils.Constants.IMAGE_VALUE_MAX
-
-        # member variables
-        self.x_cell_number = x_cell_number
-        self.y_cell_number = y_cell_number
-        self.z_cell_number = z_cell_number
-        self.bin_number = bin_number
-
-    def fit(self, X, y=None):
-
-        print("------------------------------------")
-        print("MeanIntensityGradient fit")
-        print("cell numbers = {}x{}x{}".format(self.x_cell_number,
-                                               self.y_cell_number,
-                                               self.z_cell_number))
-        print("bin numbers = {}".format(self.bin_number))
-
-        # no internal variable
-        X = check_array(X)
-
-        return self
-
-    def transform(self, X, y=None):
-
-        X = check_array(X)
-        n_samples, n_features = np.shape(X)
-
-        print("------------------------------------")
-        print("MeanIntensityGradient transform")
-        print("shape of X before transform : ")
-        print(X.shape)
-
-        X_3D = np.reshape(X, (-1,
-                              self.imageDimX,
-                              self.imageDimY,
-                              self.imageDimZ))
-
-        # cell (contains index of voxels) as bin edge
-        x_cell_edges = np.linspace(0,
-                                   self.imageDimX,
-                                   self.x_cell_number + 1,
-                                   dtype=int)
-        y_cell_edges = np.linspace(0,
-                                   self.imageDimY,
-                                   self.y_cell_number + 1,
-                                   dtype=int)
-        z_cell_edges = np.linspace(0,
-                                   self.imageDimZ,
-                                   self.z_cell_number + 1,
-                                   dtype=int)
-
-        # gradient
-        gradient = np.zeros((n_samples,
-                             self.x_cell_number,
-                             self.y_cell_number,
-                             self.z_cell_number, 3))
-
-        for i in range(0, n_samples):
-            image_3D = X_3D[i, :, :, :]
-
-            for xi in range(0, x_cell_edges.size - 1):
-                for yi in range(0, y_cell_edges.size - 1):
-                    for zi in range(0, z_cell_edges.size - 1):
-
-                        # image block for histogram
-                        image_block = image_3D[
-                                      x_cell_edges[xi]:x_cell_edges[xi+1],
-                                      y_cell_edges[yi]:y_cell_edges[yi+1],
-                                      z_cell_edges[zi]:z_cell_edges[zi+1]]
-
-                        # block gradient
-                        image_gradient = np.gradient(image_block)
-
-                        gradient[i, xi, yi, zi, 0] = np.mean(image_gradient[0])
-                        gradient[i, xi, yi, zi, 1] = np.mean(image_gradient[1])
-                        gradient[i, xi, yi, zi, 2] = np.mean(image_gradient[2])
-
-        X_new = np.reshape(gradient, (n_samples, -1))
-
-        print("shape of X after transform : ")
-        print(X_new.shape)
 
         return X_new
 
@@ -619,6 +330,28 @@ class SiftAllAxis(BaseEstimator, TransformerMixin):
         if self.verbosity > 0:
             print("shape of X after transform : ")
             print(X_new.shape)
+
+        return X_new
+
+
+class ImageHistogramAndSift(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.sift = SiftAllAxis()
+        self.image_hist = IntensityHistogram(bin_number=60)
+
+    def fit(self, X, y=None):
+        # no internal variable
+        X = check_array(X)
+
+        return self
+
+
+    def transform(self, X, y=None):
+        X = check_array(X)
+        n_samples, n_features = np.shape(X)
+
+        X_new = np.hstack((self.sift.transform(X, y),
+                           self.image_hist.transform(X, y)))
 
         return X_new
 
