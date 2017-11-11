7a29d170bbc6cfb54cc88f5e422a4c1026f155c8
Dongho Kang <east0822@gmail.com>
2017-11-11 14:32:01 +0100

    3d hog implementing...

--- ml_project/models/feature_extraction.py
+++ ml_project/models/feature_extraction.py
@@ -111,6 +111,111 @@ class IntensityHistogram(BaseEstimator, TransformerMixin):
         return X_new
 
 
+class GradientHistogram(BaseEstimator, TransformerMixin):
+    """Feature from intensity histogram of 3D images"""
+
+    # divide 3d image into cells and make histogram per cell
+    def __init__(self,
+                 x_cell_number=8,
+                 y_cell_number=8,
+                 z_cell_number=8,
+                 bin_number=60,
+                 verbosity=1):
+
+        # image dimension
+        self.imageDimX = utils.Constants.IMAGE_DIM_X
+        self.imageDimY = utils.Constants.IMAGE_DIM_Y
+        self.imageDimZ = utils.Constants.IMAGE_DIM_Z
+        self.histBinMax = utils.Constants.IMAGE_VALUE_MAX
+
+        # member variables
+        self.x_cell_number = x_cell_number
+        self.y_cell_number = y_cell_number
+        self.z_cell_number = z_cell_number
+        self.bin_number = bin_number
+
+        # verbosity
+        self.verbosity = verbosity
+
+    def fit(self, X, y=None):
+        if self.verbosity > 0:
+            print("------------------------------------")
+            print("GradientHistogram fit")
+            print("cell numbers = {}x{}x{}".format(self.x_cell_number,
+                                                   self.y_cell_number,
+                                                   self.z_cell_number))
+            print("bin numbers = {}".format(self.bin_number))
+
+        # no internal variable
+        X = check_array(X)
+
+        return self
+
+    def transform(self, X, y=None):
+
+        X = check_array(X)
+        n_samples, n_features = np.shape(X)
+
+        if self.verbosity > 0:
+            print("------------------------------------")
+            print("GradientHistogram transform")
+            print("shape of X before transform : ")
+            print(X.shape)
+
+        X_3D = np.reshape(X, (-1,
+                              self.imageDimX,
+                              self.imageDimY,
+                              self.imageDimZ))
+
+        # cell (contains index of voxels) as bin edge
+        x_cell_edges = np.linspace(0,
+                                   self.imageDimX,
+                                   self.x_cell_number + 1,
+                                   dtype=int)
+        y_cell_edges = np.linspace(0,
+                                   self.imageDimY,
+                                   self.y_cell_number + 1,
+                                   dtype=int)
+        z_cell_edges = np.linspace(0,
+                                   self.imageDimZ,
+                                   self.z_cell_number + 1,
+                                   dtype=int)
+
+        # histograms
+        histogram = np.zeros((n_samples,
+                              self.x_cell_number,
+                              self.y_cell_number,
+                              self.z_cell_number,
+                              self.bin_number))
+
+        for i in range(0, n_samples):
+            image_3D = X_3D[i, :, :, :]
+
+            for xi in range(0, x_cell_edges.size - 1):
+                for yi in range(0, y_cell_edges.size - 1):
+                    for zi in range(0, z_cell_edges.size - 1):
+
+                        # image block for histogram
+                        image_block = image_3D[
+                                      x_cell_edges[xi]:x_cell_edges[xi+1],
+                                      y_cell_edges[yi]:y_cell_edges[yi+1],
+                                      z_cell_edges[zi]:z_cell_edges[zi+1]]
+
+                        # histogram
+                        histogram[i, xi, yi, zi, :], bins = \
+                            np.histogram(image_block,
+                                         bins=np.linspace(0,
+                                                          self.histBinMax,
+                                                          self.bin_number + 1))
+
+        X_new = np.reshape(histogram, (n_samples, -1))
+
+        if self.verbosity > 0:
+            print("shape of X after transform : ")
+            print(X_new.shape)
+
+        return X_new
+
 class SiftDetector(BaseEstimator, TransformerMixin):
     """Sift feature for each cut (plane // XY)"""
 
